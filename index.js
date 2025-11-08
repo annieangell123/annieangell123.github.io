c729d183f5f7529d743bf6cde065970058896707
annieangell123 <anniehangell@gmail.com>
2025-11-08 11:12:45 -0800

    Add files via upload

--- index.js
+++
@@ -1,5 +0,0 @@
-const greeting = document.getElementById("greeting");
-greeting.style.textAlign = "center";
-greeting.style.marginTop = "50px";
-const username = window.prompt("what is your name?", "jane");
-greeting.textContent = "welcome to the cyborg army " + username + "!";
